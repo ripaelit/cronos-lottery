bf29dcd6c41d704be32bc0ab8d80f32ac7dd53ce
ripaelit <ripaelit1111@gmail.com>
2023-03-20 18:13:48 +0100

    Update dapp.config.js

--- dapp.config.js
+++ dapp.config.js
@@ -1,7 +1,7 @@
 const config = {
   title: 'Cro-Draw Dapp',
   description: 'cro-draw lottery',
-  contractAddress: '0xfd1D188160dF348b27a1A2dC9d2a383C033EEF8F',
+  contractAddress: '0x22AF42272Cb16D7C72A9FAf5Dd302D15323454f9',
   // contractAddress: '0x3611203aE7551bF519D48cA4901b02bFCde7Ca81',
   maxMintAmount: 10,
   presaleMaxMintAmount: 4,
