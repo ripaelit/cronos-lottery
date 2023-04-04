dfb02641094ff50baa3c298a1f7c16ce87df62c9
ripaelit <ripaelit1111@gmail.com>
2023-04-04 14:58:02 +0800

    update deployContract

--- scripts/deployContract.js
+++ scripts/deployContract.js
@@ -5,13 +5,13 @@
 
 const hre = require('hardhat')
 
-const discountTokenAddress = '0xEc738E4F4ab9D05F2D86E8008Ec7D388197EaB62'
-const witnetAddress = '0x6Eb87EcCe6218Cd0e97299331D2aa5d2e53da5cD'
-const charity = '0x1576E561F2636e090cb855277eBA6bc89FB5CAC7'
-const project = '0x1576E561F2636e090cb855277eBA6bc89FB5CAC7'
-const founder1 = '0x1576E561F2636e090cb855277eBA6bc89FB5CAC7'
-const founder2 = '0x1576E561F2636e090cb855277eBA6bc89FB5CAC7'
-const founder3 = '0x1576E561F2636e090cb855277eBA6bc89FB5CAC7'
+const discountTokenAddress = '0xeDdA73a0462630B84a4fD85E79F81327892b272c'
+const witnetAddress = '0x0017A464A86f48B342Cae3b8Fe29cFCDaA7b0643'
+const charity = '0x47b2A92Cd85488F41A7139174347612c085Cde2a'
+const project = '0x47b2A92Cd85488F41A7139174347612c085Cde2a'
+const founder1 = '0x47b2A92Cd85488F41A7139174347612c085Cde2a'
+const founder2 = '0x47b2A92Cd85488F41A7139174347612c085Cde2a'
+const founder3 = '0x47b2A92Cd85488F41A7139174347612c085Cde2a'
 
 async function main() {
   // Deploy the contract
