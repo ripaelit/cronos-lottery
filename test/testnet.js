3ff4133e71bd2e9bc49cacc47e1812bc56689544
ripaelit <ripaelit1111@gmail.com>
2023-04-05 06:07:36 +0800

    update testcase

--- test/testnet.js
+++ test/testnet.js
@@ -5,9 +5,10 @@ describe("CroDraw", function () {
   let trpz, lottery, witnetRequestBoard, witnetRandomness;
   let charity, project, founder1, founder2, founder3, operator;
   let accounts = [];
-  // const discountTokenAddress = '0xeDdA73a0462630B84a4fD85E79F81327892b272c'
-  // const witnetAddress = '0x0017A464A86f48B342Cae3b8Fe29cFCDaA7b0643'
-  const lotteryAddress = '0x887ee5f86fA6f09315Dd453Cd021F686d00D925a'
+  const discountTokenAddress = '0xeDdA73a0462630B84a4fD85E79F81327892b272c'
+  const nftAddress = '0x68A3b8Cfb303C798B0792d6E2b6B439D407B1B19'
+  const witnetAddress = '0x0017A464A86f48B342Cae3b8Fe29cFCDaA7b0643'
+  const lotteryAddress = '0xd4001855711adCeaF2F367F8b46c5b09813e8FC6'
   
   before(async () => {
     // [charity, project, founder1, founder2, founder3, operator, ...accounts] = await ethers.getSigners()
@@ -29,11 +30,12 @@ describe("CroDraw", function () {
 
   it("Distribute rewards to users", async function () {
     await (await lottery.connect(operator).setOperatorAndTreasuryAndCharityAddresses(operator.address, project.address, charity.address, founder1.address, founder2.address)).wait();
+    await (await lottery.connect(operator).setDiscountToken(discountTokenAddress)).wait();
     await (await lottery.connect(operator).setTicketPrice(1000)).wait();
+    await (await lottery.connect(operator).setDiscountNFTContractAddress(nftAddress)).wait();
     await (await lottery.connect(operator).startLottery(300)).wait();
-    await (await lottery.connect(operator).buyTickets(1, { from: operator.address, value: 1000 })).wait();
-    await (await lottery.connect(operator).buyTickets(1, { from: operator.address, value: 1000 })).wait();
-    await (await lottery.connect(operator).closeLottery({ from: operator.address, value: 1000000000000000000 })).wait();
+    await (await lottery.connect(operator).buyTickets(1, { from: operator.address, value: ethers.utils.parseEther("1") })).wait();
+    await (await lottery.connect(operator).closeLottery({ value: ethers.utils.parseEther("1") })).wait();
     await (await lottery.connect(operator).declareWinner()).wait();
     await (await lottery.connect(operator).claimRewards()).wait();
 
